d8ea390032d8d2612576f40b32d20bceff82ad44
Merzak Benaissi <merzak89merzak@gmail.com>
2024-02-19 18:20:31 +0100

    fix style syntax in React component

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -38,10 +38,10 @@ const Logo = ({ size }) => (
             <text
                 fill="url(#paint5_linear_56_57)"
                 style={{
-                    "font-family": "Kalimati",
-                    "font-size": "39px",
-                    "font-weight": 700,
-                    "white-space": "pre",
+                    "fontFamily": "Kalimati",
+                    "fontSize": "39px",
+                    "fontWeight": 700,
+                    "whiteSpace": "pre",
                 }}
                 x="-1.125"
                 y="44.995"
